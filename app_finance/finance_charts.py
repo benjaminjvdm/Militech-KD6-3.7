fd756a801b10dc99e17d61fe3eb64d7e8d55b994
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2025-05-17 19:25:29 +0000

    successful harmonics

--- app_finance/finance_charts.py
+++ app_finance/finance_charts.py
@@ -6,12 +6,12 @@ from pyharmonics.plotter import Plotter
 b = YahooCandleData()
 b.get_candles('GBPJPY=X', b.MIN_5, 1000)
 b.df
-print(b.df)
+# print(b.df)
 
 
-t = OHLCTechnicals(b.df, b.symbol, b.interval, peak_spacing=15)
+t = OHLCTechnicals(b.df, b.symbol, b.interval, peak_spacing=3)
 t.df
-print(t.df)
+# print(t.df)
 
 h = HarmonicSearch(t)
 
@@ -20,6 +20,6 @@ h.search()
 patterns = h.get_patterns()
 
 
-p = Plotter(t, 'GBPJPY=X', b.MIN_5)
-p.add_harmonic_plots(h.get_patterns(family=h.XABCD)(formed=False))
+p = Plotter(t,plot_ema=False, plot_sma=False)
+p.add_harmonic_plots(h.get_patterns(family=h.XABCD))
 p.show()
