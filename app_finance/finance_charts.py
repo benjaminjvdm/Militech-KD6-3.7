7d620b738cf560d5cadadc832e29f0a3bbc78259
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2025-05-17 18:38:53 +0000

    technicals work

--- app_finance/finance_charts.py
+++ app_finance/finance_charts.py
@@ -1,7 +1,12 @@
 from pyharmonics.marketdata import YahooCandleData
+from pyharmonics.technicals import OHLCTechnicals
+
 b = YahooCandleData()
-b.get_candles('GBPJPY=X', b.MIN_5, 1000)
+b.get_candles('GBPJPY=X', b.MIN_5, 100)
 b.df
 print(b.df)
 
 
+t = OHLCTechnicals(b.df, b.symbol, b.interval, peak_spacing=20)
+t.df
+print(t.df)
