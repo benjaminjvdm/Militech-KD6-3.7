5555452a1bfe018444b405563de68763c9282554
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2025-05-17 18:57:27 +0000

    final family set

--- app_finance/finance_charts.py
+++ app_finance/finance_charts.py
@@ -1,12 +1,25 @@
 from pyharmonics.marketdata import YahooCandleData
 from pyharmonics.technicals import OHLCTechnicals
+from pyharmonics.search import HarmonicSearch
+from pyharmonics.plotter import Plotter
 
 b = YahooCandleData()
-b.get_candles('GBPJPY=X', b.MIN_5, 100)
+b.get_candles('GBPJPY=X', b.MIN_5, 1000)
 b.df
 print(b.df)
 
 
-t = OHLCTechnicals(b.df, b.symbol, b.interval, peak_spacing=20)
+t = OHLCTechnicals(b.df, b.symbol, b.interval, peak_spacing=15)
 t.df
 print(t.df)
+
+h = HarmonicSearch(t)
+
+h.search()
+
+patterns = h.get_patterns()
+
+
+p = Plotter(t, 'GBPJPY=X', b.MIN_5)
+p.add_harmonic_plots(h.get_patterns(family=h.XABCD)(formed=False))
+p.show()
