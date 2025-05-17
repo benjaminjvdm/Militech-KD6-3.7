24ba40b7b10756978663d6ba15d9f4469dd8b13a
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2025-05-17 18:34:34 +0000

    1000 candles works

--- app_finance/finance_charts.py
+++ app_finance/finance_charts.py
@@ -1,5 +1,7 @@
 from pyharmonics.marketdata import YahooCandleData
 b = YahooCandleData()
-b.get_candles('GBPJPY=X', b.MIN_5, 100)
+b.get_candles('GBPJPY=X', b.MIN_5, 1000)
 b.df
-b.df
+print(b.df)
+
+
